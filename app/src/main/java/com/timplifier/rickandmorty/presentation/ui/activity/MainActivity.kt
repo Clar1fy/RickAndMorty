32d57c05a1d3ac5dd744ade4d079ee8516d69eff
Clar1fy <iamonmydoctorpath@gmail.com>
2022-03-16 18:24:04 +0600

    fix bug with activity

--- app/src/main/java/com/timplifier/rickandmorty/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/timplifier/rickandmorty/presentation/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.timplifier.rickandmorty
+package com.timplifier.rickandmorty.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.timplifier.rickandmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
