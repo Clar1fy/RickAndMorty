1831328a1af415fa363b417153d9f727e5563114
Clar1fy <iamonmydoctorpath@gmail.com>
2022-03-31 17:28:54 +0600

    add columninfo to a characterdto

--- app/src/main/java/com/timplifier/rickandmorty/data/remote/dtos/character/RickAndMortyCharacter.kt
+++ app/src/main/java/com/timplifier/rickandmorty/data/remote/dtos/character/RickAndMortyCharacter.kt
@@ -1,30 +1,41 @@
 package com.timplifier.rickandmorty.data.remote.dtos.character
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import com.timplifier.rickandmorty.base.BaseDiffModel
 
-@Entity
+@Entity(tableName = "characters")
+
 data class RickAndMortyCharacter(
     @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "character id")
     @SerializedName("id")
     override val id: Int,
+    @ColumnInfo(name = "character's name")
     @SerializedName("name")
     val name: String,
+    @ColumnInfo(name = "character's status")
     @SerializedName("status")
     val status: String,
+    @ColumnInfo(name = "species related to a character")
     @SerializedName("species")
     val species: String,
+    @ColumnInfo(name = "character's type")
     @SerializedName("type")
     val type: String,
+    @ColumnInfo(name = "character's gender")
     @SerializedName("gender")
     val gender: String,
+    @ColumnInfo(name = "character's origin")
     @SerializedName("origin")
     val origin: Origin,
+    @ColumnInfo(name = "character's location")
     @SerializedName("location")
 
     val location: Location,
+    @ColumnInfo(name = "character's image")
     @SerializedName("image")
     val image: String,
 ) : BaseDiffModel
