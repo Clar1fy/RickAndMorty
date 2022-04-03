77317cb9b7062c3ebe567d34d84f69c9df6a41e6
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-03 16:59:16 +0600

    replace hilt modules with koin module

--- app/src/main/java/com/timplifier/rickandmorty/di/NetworkModule.kt
+++ app/src/main/java/com/timplifier/rickandmorty/di/NetworkModule.kt
@@ -1,2 +1,11 @@
 package com.timplifier.rickandmorty.di
 
+import com.timplifier.rickandmorty.data.remote.RetrofitClient
+import org.koin.dsl.module
+
+
+private val networkModule = module {
+
+    single { RetrofitClient() }
+
+}
---
+++ app/src/main/java/com/timplifier/rickandmorty/di/RepositoryModule.kt
@@ -0,0 +1,2 @@
+package com.timplifier.rickandmorty.di
+
